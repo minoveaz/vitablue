4a88da41642212237306c864820676c3a65e732b
minoveaz <ing.miller.vega@gmail.com>
2026-08-03 16:38:37 +0000

    Deploying to deploy from @ minoveaz/vitablue@146d965d989342046cce425d1fbc36d6f07c73f9 🚀

--- assets/ui-icons-atw5Avz0.js
+++ assets/ui-icons-atw5Avz0.js
@@ -1,4 +1,4 @@
-import{r as n}from"./vendor-phvZBqgv.js";/**
+import{r as n}from"./vendor-BLlKm2hr.js";/**
  * @license lucide-react v0.562.0 - ISC
  *
  * This source code is licensed under the ISC license.
