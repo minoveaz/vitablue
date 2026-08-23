9e28329224e38687ed5d939805c879d4d6dd11a6
Miller Camilo Vega Diaz <minoveaz@macbook-air-de-miller-2.home>
2026-08-23 20:08:55 +0200

    seo: align canonical alternate hreflang tags across all pages

--- pages/public/StudentInsurance.tsx
+++ pages/public/StudentInsurance.tsx
@@ -418,9 +418,9 @@ export const StudentInsurance: React.FC = () => {
         <link rel="canonical" href={canonicalUrl} />
 
         {/* Multilingual Alternate Links */}
-        <link rel="alternate" hrefLang="es" href="https://www.vitablue.es/productos/seguros-salud/seguros-sanitas/international-students" />
+        <link rel="alternate" hrefLang="es" href="https://www.vitablue.es/productos/seguros-salud/seguro-medico-estudiantes" />
         <link rel="alternate" hrefLang="en" href="https://www.vitablue.es/en/health-insurance-student-visa-spain" />
-        <link rel="alternate" hrefLang="x-default" href="https://www.vitablue.es/productos/seguros-salud/seguros-sanitas/international-students" />
+        <link rel="alternate" hrefLang="x-default" href="https://www.vitablue.es/productos/seguros-salud/seguro-medico-estudiantes" />
 
         {/* Open Graph / Facebook */}
         <meta property="og:type" content="website" />
